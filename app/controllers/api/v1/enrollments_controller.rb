f1b916c39abe2ccea101ea256377a5d6f7b07955
Thiago Henrique <thiago.leitesilva05@gmail.com>
2021-03-28 22:28:59 -0300

    update_controllers_and_exceptions (#1)

--- app/controllers/api/v1/enrollments_controller.rb
+++ app/controllers/api/v1/enrollments_controller.rb
@@ -1,43 +1,49 @@
 module Api
   module V1
     class EnrollmentsController < ApplicationController
-      rescue_from ActiveRecord::RecordNotFound, with: :not_found
+      include ExceptionTreatable
 
       # List enabled enrollments
       def index
-        enrollments = Enrollment.where('enabled_enr = ?', true).order('id ASC')
+        enrollments = Enrollment.where(enabled_enr: true)
+
         render json: { message: 'All enrollments loaded.', data: enrollments }, status: :ok
       end
 
       # List enrollments of a student
       def get_by_students
-        params[:student_id]
-        enrollments = Enrollment.joins(:student).where('student_id = ?', params[:student_id]).where('enabled_enr = ?', true).order('id ASC')
-        render json: { message: "Enrollments of student #{params[:student_id]} loaded.", data: enrollments }, status: :ok
+        student_id = params[:student_id]
+        enrollments = Enrollment.joins(:student).where(student_id: student_id, enabled_enr: true)
+
+        render json: { message: "Enrollments of student #{student_id} loaded.", data: enrollments }, status: :ok
       end
 
       # List enrollments of a institution
       def get_by_institutions
-        params[:institution_id]
-        enrollments = Enrollment.joins(:institution).where('institution_id = ?', params[:institution_id]).where('enabled_enr = ?', true).order('id ASC')
-        render json: { message: "Enrollments of institution #{params[:institution_id]} loaded.", data: enrollments }, status: :ok
+        institution_id = params[:institution_id]
+        enrollments = Enrollment.joins(:institution).where(institution_id: institution_id, enabled_enr: true)
+
+        render json: { message: "Enrollments of institution #{institution_id} loaded.", data: enrollments }, status: :ok
       end
 
       # List enabled and disabled enrollments
       def get_all
-        enrollments = Enrollment.order('id ASC')
+        enrollments = Enrollment.all
+
         render json: { message: 'All enrollments loaded. Enabled and Disabled.', data: enrollments }, status: :ok
       end
 
       # List disabled enrollments
       def get_disabled
-        enrollments = Enrollment.where('enabled_enr = ?', false).order('id ASC')
+        enrollments = Enrollment.where(enabled_enr: false)
+
         render json: { message: 'All disabled enrollments loaded.', data: enrollments }, status: :ok
       end
 
       # List specific enrollment by enrollment ID
       def show
-        enrollment = Enrollment.where('enabled_enr = ?', true).find(params[:id])
+        enrollment = Enrollment.find(params[:id])
+
         render json: { message: "Enrollment #{params[:id]} loaded.", data: enrollment }, status: :ok
       end
 
@@ -54,16 +60,12 @@ module Api
       # Updates a enrollment
       def update
         id = params[:id]
-        enrollment = Enrollment.where('enabled_enr = ?', true).find(id)
+        enrollment = Enrollment.find(id)
         enrollment.update_attributes(enrollment_params)
+
         if enrollment_params.empty?
           render json: { message: "Enrollment #{id} selected. Enter the parameters to be updated", data: enrollment }, status: :ok
         else
-          # Disables bills with old parameters and creates new ones
-          Bill.where('enrollment_id = ?', id).update_all(enabled_bill: false)
-
-          enrollment = Enrollment.find(id)
-          enrollment.create_bills
           render json: { message: "Enrollment #{id} updated.", data: enrollment }, status: :ok
         end
       end
@@ -71,11 +73,10 @@ module Api
       # Delete an enrollment
       def destroy
         ActiveRecord::Base.transaction do
-          enrollment = Enrollment.where('enabled_enr = ?', true).find(params[:id])
-
-          bills = Bill.where('enrollment_id = ?', params[:id]).update_all(enabled_bill: false)
-
+          enrollment = Enrollment.find(params[:id])
           enrollment.update(enabled_enr: false)
+          disable_bills(enrollment)
+
           render json: { message: "Enrollment #{params[:id]} deleted.", data: enrollment }, status: :ok
         end
       end
@@ -89,6 +90,10 @@ module Api
 
       def not_found
         render json: { message: 'Error: Not Found. Enrollment Id does not exist.' }, status: :not_found
+      end
+
+      def disable_bills(enrollment)
+        Bill.where(enrollment_id: enrollment.id).update_all(enabled_bill: false)
       end
     end
   end
--- app/controllers/api/v1/students_controller.rb
+++ app/controllers/api/v1/students_controller.rb
@@ -1,36 +1,40 @@
 module Api
   module V1
     class StudentsController < ApplicationController
-      rescue_from ActiveRecord::RecordNotFound, with: :not_found
-      rescue_from ActiveRecord::RecordNotUnique, with: :not_unique
+      include ExceptionTreatable
 
       # List enabled students
       def index
-        students = Student.where('enabled_std = ?', true).order('id ASC')
+        students = Student.where(enabled_std: true)
+
         render json: { message: 'All students loaded.', data: students }, status: :ok
       end
 
       # List specific student by student ID
       def show
-        student = Student.where('enabled_std = ?', true).find(params[:id])
+        student = Student.find(params[:id])
+
         render json: { message: "Student #{params[:id]} loaded.", data: student }, status: :ok
       end
 
       # List enabled and disabled students
       def get_all
-        students = Student.order('id ASC')
+        students = Student.all
+
         render json: { message: 'All students loaded. Enabled and Disabled.', data: students }, status: :ok
       end
 
       # List disabled students
       def get_disabled
-        students = Student.where('enabled_std = ?', false).order('id ASC')
+        students = Student.where(enabled_std: false)
+
         render json: { message: 'All disabled students loaded.', data: students }, status: :ok
       end
 
       # Create a new student
       def create
         student = Student.new(student_params)
+
         if student.save
           render json: { message: 'Student registered.', data: student }, status: :ok
         else
@@ -40,13 +44,12 @@ module Api
 
       # Updates a student
       def update
-        student = Student.where('enabled_std = ?', true).find(params[:id])
+        student = Student.find(params[:id])
         student.update_attributes(student_params)
 
         if student_params.empty?
           render json: { message: "Student #{params[:id]} selected. Enter the parameters to be updated.", data: student }, status: :ok
         else
-          student = Student.find(params[:id])
           render json: { message: "Student #{params[:id]} updated.", data: student }, status: :ok
         end
       end
@@ -54,12 +57,11 @@ module Api
       # Delete an student
       def destroy
         ActiveRecord::Base.transaction do
-          student = Student.where('enabled_std = ?', true).find(params[:id])
-
-          bills = Bill.where('student_id = ?', params[:id]).update_all(enabled_bill: false)
-          enrollments = Enrollment.where('student_id = ?', params[:id]).update_all(enabled_enr: false)
-
+          student = Student.where(enabled_std: true).find(params[:id])
           student.update(enabled_std: false)
+          disable_enrollments(student)
+          disable_bills(student)
+
           render json: { message: "Student #{params[:id]} deleted.", data: student }, status: :ok
         end
       end
@@ -71,12 +73,12 @@ module Api
         params.permit(:name, :cpf, :birth_date, :phone, :gender, :pay_method, :cep)
       end
 
-      def not_found
-        render json: { message: 'Error: Not Found. Student Id does not exist.' }, status: :not_found
+      def disable_bill(student)
+        Bill.where('student_id = ?', params[:id]).update_all(enabled_bill: false)
       end
 
-      def not_unique
-        render json: { message: 'Error: Not Unique. Name or CPF already exists.' }, status: :conflict
+      def disable_enrollments(student)
+        Enrollment.where('student_id = ?', params[:id]).update_all(enabled_enr: false)
       end
     end
   end
